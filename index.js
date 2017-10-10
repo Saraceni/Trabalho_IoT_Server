e04a09a85345cd222a18e425c3d1a0f3df3932ad
Saraceni <saraceni.br@gmail.com>
2017-10-09 23:39:34 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/newData', function(req, res) {
+    console.log(req.body.status);
+    res.send("OK");
+    io.emit('new_status', {status: req.body.status});
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
